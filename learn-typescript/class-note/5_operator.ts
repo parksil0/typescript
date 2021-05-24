7881fc9469f69897e8c0eaa5263e4ac2d4241beb
parksil0 <parksil00@naver.com>
2021-05-24 22:33:30 +0900

    연산자

--- learn-typescript/class-note/5_operator.ts
+++ learn-typescript/class-note/5_operator.ts
@@ -34,12 +34,21 @@ interface Person {
 
 // function askSomeone(someone: Developer | Person) {
 //   // 각 유니온 타입안의 속성이 공통된 부분만 타입을 통과시키고, 나머지는 다른 로직이 필요하다.
-//   someone.name;
+//   // someone.name;
 // }
 
+// askSomeone({ name: 'Developer', skill: '웹 개발'});
+// askSomeone({ name: 'seal', age: 28});
+
 function askSomeone(someone: Developer & Person) {
-  
+  someone.name;
+  someone.age;
+  someone.skill;
 }
 
+askSomeone({ name: 'Developer', skill: '웹 개발', age: 28 });
+askSomeone({ name: 'seal', skill: 'Web development', age: 28 });
+
 var seal : string | number | boolean;
+// intersection(인터섹션) = &
 var seal2 : string & number & boolean;
